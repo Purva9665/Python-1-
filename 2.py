d7d89b897d8be1c9928303a2e39a005a3b9aefa6
Purva9665 <purvakadam9637@gmail.com>
2025-06-28 00:49:09 +0530

    Add files via upload
    task 1 is the first task assigned about Performing Basic Mathematical Operations.
    task 2 is the second task assigned about Creating a Personalized Greeting.

---
+++ 2.py
@@ -0,0 +1,10 @@
+a1=int(input("Enter first number: "))
+a2=int(input("Enter second number: "))
+add=a1+a2
+sub=a1-a2
+mult=a1*a2
+div=a1/a2
+print("Addition: ",add)
+print("Substraction :",sub)
+print("Multiplication: ",mult)
+print("Division: ",div)
